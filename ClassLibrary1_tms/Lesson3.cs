546e2fa49897736bad2879b19b57600ce65a128a
Andrey <andreymoiseenkohub@gmail.com>
2024-12-20 01:04:48 +0300

    add ShowSeasonIf method. rename the "ShowSeason" method to "ShowSeasonSwitch"

--- ClassLibrary1_tms/Lesson3.cs
+++ ClassLibrary1_tms/Lesson3.cs
@@ -2,7 +2,7 @@ namespace ClassLibrary1_tms;
 
 public class Lesson3
 {
-    public void ShowSeason(int num)
+    public void ShowSeasonSwitch(int num)
     {
         var result = num switch
         {
@@ -18,8 +18,41 @@ public class Lesson3
             10 => "Autumn",
             11 => "Autumn",
             12 => "Winter",
-            _=>null
+            _=>"Error"
         };
-        Console.WriteLine(result ?? "Error");
+        Console.WriteLine(result);
+    }
+
+    public void ShowSeasonIf(int num)
+    {
+        string result;
+        if (num == 1)
+            result = "Winter";
+        else if (num == 2)
+            result = "Winter";
+        else if (num == 3)
+            result = "Spring";
+        else if (num == 4)
+            result = "Spring";
+        else if (num == 5)
+            result = "Spring";
+        else if (num == 6)
+            result = "Summer";
+        else if (num == 7)
+            result = "Summer";
+        else if (num == 8)
+            result = "Summer";
+        else if (num == 9)
+            result = "Autumn";
+        else if (num == 10)
+            result = "Autumn";
+        else if (num == 11)
+            result = "Autumn";
+        else if (num == 12)
+            result = "Winter";
+        else
+            result = "Error";
+        Console.WriteLine(result);
+        
     }
 }
