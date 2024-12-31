ad303a28e53ec39f04b53714104772cd3a0a90f8
Andrey <andreymoiseenkohub@gmail.com>
2024-12-31 21:19:46 +0300

    add Phone.cs

--- ConsoleApp1_tms/Program.cs
+++ ConsoleApp1_tms/Program.cs
@@ -1,4 +1,5 @@
 ﻿using ClassLibrary1_tms;
+using OOPPhone;
 
 namespace ConsoleApp1_tms;
 
@@ -10,6 +11,8 @@ class Program
         //class1.Foo();
         //Console.WriteLine(class1.GetRandomVerses(new List<string>() { "hello", "class", "hi", "one", "two" }));
         //class1.TestQuest();
-        class1.StoreSimulator();
+       // class1.StoreSimulator();
+       var phone = new Phone("asda", "asdsa");
+       Console.WriteLine(phone.Wight);
     }
 }
