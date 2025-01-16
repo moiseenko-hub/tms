fced073626bf494b2f17b5300f7f4d18b6733593
Andrey <andreymoiseenkohub@gmail.com>
2025-01-16 20:04:27 +0300

    add exception task

---
+++ ExceptionsLesson/UserValidator.cs
@@ -0,0 +1,38 @@
+﻿namespace ExceptionsLesson;
+
+public class UserValidator
+{
+    public static bool IsValid(string Login, string Password, string ConfirmPassword)
+    {
+        try
+        {
+            Validate(Login, Password, ConfirmPassword);
+        }
+        catch (UserDataException)
+        {
+            // log
+            return false;
+        }
+
+        return true;
+    }
+
+    private static void Validate(string Login, string Password, string ConfirmPassword)
+    {
+        if (Login.Length >= 20 || Login.Contains(' '))
+            throw new WrongLoginException("Wrong login exception. ");
+        if (Password.Length >= 20 || Password.Contains(' ') || !ContainsNumber(Password) || !Password.Equals(ConfirmPassword))
+            throw new WrongPasswordException("Wrong password exception"); 
+    }
+
+    private static bool ContainsNumber(string value)
+    {
+        foreach (var item in value)
+        {
+            if (Char.IsDigit(item))
+                return true;
+        }
+
+        return false;
+    }
+}
